6e9e4c6b65283978f29f161613ee0481386de825
Kamil Gabryjelski <kamil.gabryjelski@gmail.com>
2017-05-26 20:26:13 +0200

    estimations run on a copy of error cov matrix

--- Kalman.cpp
+++ Kalman.cpp
@@ -2,9 +2,9 @@
 
 using namespace boost::numeric::ublas;
 
-mat invert_matrix(const mat& input_matrix) {
-    mat A(input_matrix);
-    mat output_matrix(A.size1(), A.size1());
+matrix<double> invert_matrix(const matrix<double>& input_matrix) {
+    matrix<double> A(input_matrix);
+    matrix<double> output_matrix(A.size1(), A.size1());
     permutation_matrix<std::size_t> pm(A.size1());
     lu_factorize(A, pm);
     output_matrix.assign(identity_matrix<double> (A.size1()));
@@ -12,8 +12,11 @@ mat invert_matrix(const mat& input_matrix) {
     return output_matrix;
 }
 
-Kalman::Kalman(mat A, mat H, mat P, mat Q, mat R) : prediction_matrix(A),  observation_model(H), error_covariance(P),
-                                                    process_noise_covariance(Q), sensor_noise_covariance(R) {};
+Kalman::Kalman(mat prediction_matrix, mat observation_model, mat initial_error_covariance, mat process_noise_covariance,
+               mat sensor_noise_covariance) :
+        prediction_matrix(prediction_matrix), observation_model(observation_model),
+        initial_error_covariance(initial_error_covariance), process_noise_covariance(process_noise_covariance),
+        sensor_noise_covariance(sensor_noise_covariance) {};
 
 void Kalman::predict(vec& X, mat& P) {
     X = prod(prediction_matrix, X);
@@ -28,20 +31,21 @@ void Kalman::correct(vec& X, const vec& Y, mat& P) {
     P = prod((identity_matrix<double> (P.size1()) - prod(K, observation_model)), P);
 }
 
-void Kalman::execute_step(vec& X, const vec& Y) {
-    predict(X, error_covariance);
-    correct(X, Y, error_covariance);
+void Kalman::execute_step(vec& X, const vec& Y, mat& P) {
+    predict(X, P);
+    correct(X, Y, P);
 }
 
 std::vector<double> Kalman::estimate_position(const std::vector<double>& measured_position,
                                               const std::vector<double>& measured_velocity) {
     vec X(3, 0); // state: position, velocity, velocity_offset
     vec Y(2); // measurements: position, velocity
+    mat error_covariance(initial_error_covariance);
     std::vector<double> x_predictions;
     x_predictions.push_back(X(0));
     for (int i = 1; i < measured_position.size(); i++) {
         Y(0) = measured_position[i]; Y(1) = measured_velocity[i];
-        execute_step(X, Y);
+        execute_step(X, Y, error_covariance);
         x_predictions.push_back(X(0));
     }
     return x_predictions;
--- Kalman.h
+++ Kalman.h
@@ -4,20 +4,20 @@
 #include <boost/numeric/ublas/lu.hpp>
 #include <boost/numeric/ublas/io.hpp>
 
-typedef boost::numeric::ublas::matrix<double> mat;
-typedef boost::numeric::ublas::vector<double> vec;
-
 class Kalman {
+    typedef boost::numeric::ublas::matrix<double> mat;
+    typedef boost::numeric::ublas::vector<double> vec;
 public:
-    Kalman(mat A, mat H, mat P, mat Q, mat R);
+    Kalman(mat prediction_matrix, mat observation_model, mat initial_error_covariance, mat process_noise_covariance,
+           mat sensor_noise_covariance);
     std::vector<double> estimate_position(const std::vector<double>& measured_position,
                                           const std::vector<double>& measured_velocity);
 private:
     void predict(vec& X, mat& P);
     void correct(vec& X, const vec& Y, mat& P);
-    void execute_step(vec& X, const vec& Y);
+    void execute_step(vec& X, const vec& Y, mat& P);
 
-    mat error_covariance;
+    const mat initial_error_covariance;
     const mat prediction_matrix;
     const mat process_noise_covariance;
     const mat sensor_noise_covariance;
